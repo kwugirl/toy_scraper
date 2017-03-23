2a06eebc3d6c0aa65e7f71cae020af38e023e061
Katherine Wu <kwu@newrelic.com>
2017-03-22 22:40:08 -0700

    Add form to accept user input for reviews count and name

--- app/controllers/yelp_reviews_controller.rb
+++ app/controllers/yelp_reviews_controller.rb
@@ -1,5 +1,12 @@
 class YelpReviewsController < ApplicationController
   def index
-    render html: "hello, world!"
+    render 'index'
+  end
+
+  def search
+    count = params[:reviews][:count]
+    name = params[:restaurant_name]
+
+    redirect_to root_path count: count, restaurant_name: name
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'yelp_reviews#index'
+  get '/search', to: 'yelp_reviews#search'
 end
