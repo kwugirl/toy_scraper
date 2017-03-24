b1a812451c2340563d7627e5aa65e7b342e08e91
Katherine Wu <kwu@newrelic.com>
2017-03-23 21:21:16 -0700

    Display business details when appropriate

--- app/controllers/yelp_reviews_controller.rb
+++ app/controllers/yelp_reviews_controller.rb
@@ -1,13 +1,21 @@
 class YelpReviewsController < ApplicationController
   def index
-    business = 1
-    render 'index', locals: {business: business}
+    business = nil
+    if params[:biz_url] && params[:count]
+      business = YelpBusiness.new(params[:biz_url])
+      count = params[:count].to_i
+    end
+
+    render 'index', locals: {business: business, count: count}
   end
 
   def search
     count = params[:reviews][:count]
     name = params[:restaurant_name]
+    location = "New York, NY"
 
-    redirect_to root_path count: count, restaurant_name: name
+    biz_url = YelpSearchResults.new(name, location).url_for_first
+
+    redirect_to root_path biz_url: biz_url, count: count
   end
 end
